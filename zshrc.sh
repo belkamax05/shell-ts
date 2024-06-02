aa327d993369b94fb3b1459f7777da4069b26c80
Maksym Bondarchuk <belkamax05@gmail.com>
2024-06-02 02:47:56 +0100

    current

--- zshrc.sh
+++ zshrc.sh
@@ -1,5 +1,9 @@
 # include this file into your .zshrc file
 # source ~/dev/shell-ts/zshrc.sh
+typeset -gx SHELLTSDIR="${${(%):-%N}:A:h}"
 
-alias hello="echo 'Hello World!' not implemented yet"
-# alias hello="shell-ts run-script hello"
+# alias hello="echo 'Hello World!' not implemented yet"
+alias shell-ts="echo 'shell-ts v0.0.1'"
+alias shell-ts-path="echo $PWD"
+alias hello="yarn --cwd $SHELLTSDIR hello"
+alias version="yarn --cwd $SHELLTSDIR -v"
