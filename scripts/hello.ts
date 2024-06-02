b3748b453349081706e926d63df576598eaadf6c
Maksym Bondarchuk <belkamax05@gmail.com>
2024-06-02 02:26:27 +0100

    init

---
+++ scripts/hello.ts
@@ -0,0 +1,1 @@
+console.log("This is a hello world message from a TypeScript file!");
---
+++ zshrc.sh
@@ -0,0 +1,5 @@
+# include this file into your .zshrc file
+# source ~/dev/shell-ts/zshrc.sh
+
+alias hello="echo 'Hello World!' not implemented yet"
+# alias hello="shell-ts run-script hello"
