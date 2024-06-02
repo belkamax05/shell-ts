b2ac297bc762890d3f0cb067e9e044d3fea1aea7
Maksym Bondarchuk <belkamax05@gmail.com>
2024-06-02 04:50:01 +0100

    latest

--- install.sh
+++ install.sh
@@ -1,9 +1,5 @@
-#!/bin/bash
-
-# Define the path to the shell-ts directory
 SHELL_TS_PATH=~/dev/shell-ts
 
-# Add the lines to the .zshrc file
 echo "
 SHELL_TS_PATH=$SHELL_TS_PATH
 if [[ ! -d \$SHELL_TS_PATH ]]; then
@@ -12,5 +8,4 @@ fi
 source \$SHELL_TS_PATH/zshrc.sh
 " >>~/.zshrc
 
-# Source the .zshrc file to apply the changes
 source ~/.zshrc
