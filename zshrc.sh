864f09b3f0847d90ac076fc86e80f1b20e6a1a0f
Maksym Bondarchuk <belkamax05@gmail.com>
2024-06-02 03:45:48 +0100

    latest

--- zshrc.sh
+++ zshrc.sh
@@ -3,9 +3,9 @@
 typeset -gx STS_DIR="${${(%):-%N}:A:h}"
 
 alias sts-install="yarn --cwd $STS_DIR install"
-
+alias sts-cd="cd $STS_DIR"
 alias sts-yarn="yarn --cwd $STS_DIR"
-alias sts-run="sts-yarn"
+alias sts-run="sts-yarn --silent"
 
 alias sts-dir="sts-run dir"
 alias sts-hello="sts-run hello"
