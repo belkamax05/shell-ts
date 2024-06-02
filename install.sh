5a8c855d1bab468549379fb76fdefe613f86d459
Maksym Bondarchuk <belkamax05@gmail.com>
2024-06-02 04:52:33 +0100

    latest

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 SHELL_TS_PATH=~/dev/shell-ts
 
-echo "
+if ! grep -q "SHELL_TS_PATH=$SHELL_TS_PATH" ~/.zshrc; then
+    echo "
 SHELL_TS_PATH=$SHELL_TS_PATH
 if [[ ! -d \$SHELL_TS_PATH ]]; then
     git clone git@github.com:belkamax05/shell-ts.git \$SHELL_TS_PATH
 fi
 source \$SHELL_TS_PATH/zshrc.sh
 " >>~/.zshrc
+fi
 
 source ~/.zshrc
