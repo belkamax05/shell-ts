60569a7c509743caeec4f927ad3d59a3c794461a
Maksym Bondarchuk <maksym.bondarchuk@wundermanthompson.com>
2024-06-04 04:07:59 +0100

    latest

--- scripts/sts-run.sh
+++ scripts/sts-run.sh
@@ -1,1 +1,23 @@
-echo "sts-run file"
+function stsRun {
+    local cmd="$1"
+    local param1="$2"
+    local param2="$3"
+    local ext="${cmd##*.}"
+    if [ "$cmd" == "$ext" ]; then
+        ext=""
+    fi
+
+    if [ "$ext" == "sh" ]; then
+        $STS_DIR/command/$cmd
+    elif [ "$ext" == "js" ]; then
+        echo "Js ext"
+    elif [ "$ext" == "ts" ]; then
+        echo "Ts ext"
+    else
+        echo "TODO need to search for ext from files list"
+    fi
+
+    echo "sts-run cmd=$cmd, param1=$param1, param2=$param2, ext=$ext"
+}
+
+stsRun "$@"
